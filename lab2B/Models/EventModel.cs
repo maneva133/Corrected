ccb5112c03b05d3606fe71db38bdec1e48916c8a
Maja <majamaneva51@gmail.com>
2024-04-30 02:32:38 +0200

    Lab

---
+++ lab2B/Models/EventModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace lab2B.Models
+{
+    public class EventModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(30, MinimumLength = 5)]
+        public string Location { get; set; }
+    }
+}
